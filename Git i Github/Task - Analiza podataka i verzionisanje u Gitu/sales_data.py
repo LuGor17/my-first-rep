bb41f7ae845a4aa2614b69c6d765f3f734fe0b1f
Goran <lukajic.gorann@gmail.com>
2025-05-13 18:11:27 +0200

    Implemented total and average sales functions

--- Git i Github/Task - Analiza podataka i verzionisanje u Gitu/sales_data.py
+++ Git i Github/Task - Analiza podataka i verzionisanje u Gitu/sales_data.py
@@ -1,5 +1,5 @@
 # sales_data.py
-# Weekly sales data: [Ponedjeljak, Utorak, Srijeda, Četvrtak, Petak, Subota, Sunday]
+# Weekly sales data: [Ponedjeljak, Utorak, Srijeda, Četvrtak, Petak, S, Sunday]
 sales_data = [230, 200, 310, 290, 400, 150, 180]
 
 def total_sales(data):
